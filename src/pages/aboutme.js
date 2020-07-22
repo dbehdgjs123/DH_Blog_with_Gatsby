c0ed87814e01175fee42a8bcc7ae170bec40d551
dbehdgjs123 <dbehdgjs12345@gmail.com>
2020-07-23 03:51:03 +0900

    스타일 변경

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -6,13 +6,15 @@ function Aboutme() {
   return (
     <Layout>
       <div className="about-container">
-        <FaQuoteLeft />
-        <span>
-          현재 ES6,React,Nodejs,express를 중점으로 공부중입니다. <br />
-          실력도 중요하지만 집념과 끈기가 바탕이 되는 개발자를 목표로 하고
-          있습니다.
-        </span>
-        <FaQuoteRight />
+        <div className="about-text">
+          <FaQuoteLeft />
+          <span>
+            현재 ES6,React,Nodejs,express를 중점으로 공부중입니다. <br />
+            실력도 중요하지만 집념과 끈기가 바탕이 되는 개발자를 목표로 하고
+            있습니다.
+          </span>
+          <FaQuoteRight />
+        </div>
       </div>
     </Layout>
   );
