5b65628628ab3f52eac5d98d6cb2abdf5a2ad16c
dbehdgjs123 <dbehdgjs12345@gmail.com>
2020-07-28 03:15:55 +0900

    리덕스 상태관리2 포스트  업로드

--- src/context/GlobalContextProvider.js
+++ src/context/GlobalContextProvider.js
@@ -5,28 +5,6 @@ import PropTypes from "prop-types";
 export const GlobalStateContext = createContext();
 export const GlobalDispatchContext = createContext();
 
-/*function getInitialColor() {
-  if (typeof window !== `undefined`) {
-    const themeColor = window.localStorage.getItem("themeColor"); //로컬 스토리지에서 theme키를 가져온다.
-    const hasThemeColor = typeof themeColor === "string";
-
-    if (hasThemeColor) {
-      return themeColor;
-    }
-
-    const mql = window.matchMedia("(prefers-color-scheme: dark)"); //사용자의 시스템이 라이트인지 다크인지를 가져온다.
-    const hasMediaQueryPreference = typeof mql.matches === "boolean";
-
-    if (hasMediaQueryPreference) {
-      return mql.matches ? "dark" : "light";
-    }
-
-    return "light"; //아무것도 아니면 기본인 light를 테마로 사용한다.
-  } else {
-    return "light";
-  }
-}*/
-
 const initialState = {
   theme: typeof window !== "undefined" ? window.__theme : "light",
 };
