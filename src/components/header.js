b7a379da2fb264ec0575bd83fdfcd5706ef261fc
dbehdgjs123 <dbehdgjs12345@gmail.com>
2020-08-05 00:59:34 +0900

    헤더 스타일 아이폰 수정

--- src/components/header.js
+++ src/components/header.js
@@ -82,41 +82,42 @@ const Header = ({
         <Link to="/aboutme" className="about_btn">
           About me
         </Link>
-      </div>
-      <div className="contect_sns">
-        <button onClick={showSns} className={sns}>
-          <FaAngleDown />
-        </button>
-        <div className="contect_sns_list">
-          <a
-            className={`${sns} ${snsTransition}`}
-            href="https://www.instagram.com/yu_dongheon/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaInstagram />
-          </a>
-          <a
-            className={`${sns} ${snsTransition}`}
-            href="https://github.com/dbehdgjs123"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGithub />
-          </a>
-          <a
-            className={`${sns} ${snsTransition}`}
-            href="https://www.facebook.com/profile.php?id=100006436004409"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaFacebook />
-          </a>
-          <Link to="/aboutme" className={`${sns} ${snsTransition}`}>
-            <FaUserCircle />
-          </Link>
+        <div className="contect_sns">
+          <button onClick={showSns} className={sns}>
+            <FaAngleDown />
+          </button>
+          <div className="contect_sns_list">
+            <a
+              className={`${sns} ${snsTransition}`}
+              href="https://www.instagram.com/yu_dongheon/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram />
+            </a>
+            <a
+              className={`${sns} ${snsTransition}`}
+              href="https://github.com/dbehdgjs123"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+            </a>
+            <a
+              className={`${sns} ${snsTransition}`}
+              href="https://www.facebook.com/profile.php?id=100006436004409"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook />
+            </a>
+            <Link to="/aboutme" className={`${sns} ${snsTransition}`}>
+              <FaUserCircle />
+            </Link>
+          </div>
         </div>
       </div>
+
       <div className="right_menu">
         <div
           className="right_menu_theme"
